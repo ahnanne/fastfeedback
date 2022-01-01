fb2bedf75df2a1f284a8e11dada62f3799998ac2
ahnanne <54733637+ahnanne@users.noreply.github.com>
2022-01-02 04:27:00 +0900

    chore: custom document

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
 import { AuthProvider } from '../lib/auth';
+import { ThemeProvider } from '@emotion/react';
 
 import '../styles/globals.css';
+import theme from '../styles/theme';
+
 /**
  * https://nextjs.org/docs/advanced-features/custom-app
  */
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
 
---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+/** https://nextjs.org/docs/advanced-features/custom-document */
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
