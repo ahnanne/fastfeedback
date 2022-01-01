fe7ee7b691d126fc628fc4b0315dca39bd003afd
ahnanne <54733637+ahnanne@users.noreply.github.com>
2022-01-02 03:59:05 +0900

    feat: chakra ui custom theme

--- utils/formatUser.js
+++ utils/formatUser.js
@@ -1,9 +1,11 @@
 const formatUser = (user) => {
+  console.log(user);
   return {
     uid: user.uid,
     email: user.email,
     name: user.displayName,
-    provider: user.providerData[0].providerId
+    provider: user.providerData[0].providerId,
+    photoUrl: user.photoURL
   };
 };
 
