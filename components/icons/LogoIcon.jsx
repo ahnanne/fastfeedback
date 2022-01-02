12cde5d157451377232e3f7f1422e5917d67ab26
ahnanne <54733637+ahnanne@users.noreply.github.com>
2022-01-03 03:28:54 +0900

    feat: main page layout

---
+++ components/icons/LogoIcon.jsx
@@ -0,0 +1,12 @@
+import { createIcon } from '@chakra-ui/icons';
+
+export const LogoIcon = createIcon({
+  displayName: 'LogoIcon',
+  viewBox: '0 0 201 100',
+  path: (
+    <g>
+      <circle cx="50" cy="50" r="50" fill="#6667ab" />
+      <circle cx="167.5" cy="50.5" r="33.5" fill="#E1D1FF" />
+    </g>
+  )
+});
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head';
 import { useAuth } from '@/lib/auth';
 
-import { Button, Heading, Text, Code } from '@chakra-ui/react';
+import { Button, Heading, Text, Code, Flex } from '@chakra-ui/react';
+import { LogoIcon } from '@/components/icons/LogoIcon';
 
 export default function Home() {
   const auth = useAuth();
@@ -12,30 +13,33 @@ export default function Home() {
         <title>Fast Feedback</title>
       </Head>
       <div>
-        <main>
+        <Flex
+          as="main"
+          direction="column"
+          align="center"
+          justify="center"
+          w="100vw"
+          height="100vh"
+        >
           <Heading>Fast Feedback</Heading>
-
+          <LogoIcon w={101} h={20} />
           {auth?.user ? (
-            <>
-              {auth?.user?.name?.split(' ')[0] ? (
-                <Text>
-                  Welcome back, {auth?.user?.name?.split(' ')[0]}!
-                  <br />
-                  (current user: <Code>{auth?.user?.email}</Code>)
-                </Text>
-              ) : (
-                <>Welcome back!</>
-              )}
-              <Button onClick={(e) => auth.signout()}>Sign Out</Button>
-            </>
+            <Button onClick={(e) => auth.signout()}>Sign Out</Button>
           ) : (
             <>
-              <Button onClick={(e) => auth.signinWithGithub()}>Sign In</Button>
+              <Button
+                variant="link"
+                size="sm"
+                mt={4}
+                onClick={(e) => auth.signinWithGithub()}
+              >
+                Sign In
+              </Button>
               {/* 인증 사용자 확인은 아래 링크에서. */}
               {/* https://console.firebase.google.com/u/0/project/fast-feedback-demo-b442c/authentication/users?hl=ko */}
             </>
           )}
-        </main>
+        </Flex>
       </div>
     </>
   );
