604485bc5ec49ae6d3d99cc5ccc45c050d5b37dd
YIAHN404-FASTFIVE <54733637+ahnanne@users.noreply.github.com>
2022-04-29 15:31:20 +0900

    input 4개일 때

--- pages/payment.js
+++ pages/payment.js
@@ -5,8 +5,11 @@ export default function Payment() {
 
   return (
     <>
-      <label htmlFor="cardnumber">카드 번호</label>
-      <input type="text" id="cardnumber" name="cardnumber" value={cardNumber} onChange={(e) => setCardNumber(e.target.value)} />
+      <label htmlFor="cardnumber1">카드 번호</label>
+      <input type="text" id="cardnumber1" name="cardnumber" value={cardNumber} onChange={(e) => setCardNumber(e.target.value)} />
+      <input type="text" id="cardnumber2" name="cardnumber" value={cardNumber} onChange={(e) => setCardNumber(e.target.value)} />
+      <input type="text" id="cardnumber3" name="cardnumber" value={cardNumber} onChange={(e) => setCardNumber(e.target.value)} />
+      <input type="text" id="cardnumber4" name="cardnumber" value={cardNumber} onChange={(e) => setCardNumber(e.target.value)} />
     </>
   );
 }
