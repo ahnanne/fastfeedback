6fd4e28f5db2c34f2e380aff7c1e95f590377a8d
YIAHN404-FASTFIVE <54733637+ahnanne@users.noreply.github.com>
2022-04-29 16:10:49 +0900

    신용 카드 스캔되면 각각의 input value 찍어보기

--- pages/payment.js
+++ pages/payment.js
@@ -1,18 +1,51 @@
 import { useState } from 'react';
 
 export default function Payment() {
-  const [cardNumber, setCardNumber] = useState('');
+  // const [cardNumber, setCardNumber] = useState('');
 
   return (
     <>
       <label htmlFor="cardnumber1">카드 번호</label>
-      <input type="text" id="cardnumber1" name="cardnumber" value={cardNumber} onChange={(e) => {
-        console.log(e.target.value);
-        setCardNumber(e.target.value);
-      }} autoComplete="cc-number" maxLength={4} />
-      <input type="text" id="cardnumber2" name="cardnumber" value={cardNumber} onChange={(e) => setCardNumber(e.target.value)} autoComplete="cc-number" />
-      <input type="text" id="cardnumber3" name="cardnumber" value={cardNumber} onChange={(e) => setCardNumber(e.target.value)} autoComplete="cc-number" />
-      <input type="text" id="cardnumber4" name="cardnumber" value={cardNumber} onChange={(e) => setCardNumber(e.target.value)} autoComplete="cc-number" />
+      <input
+        type="text"
+        id="cardnumber1"
+        name="cardnumber"
+        onChange={(e) => {
+          console.log('cardnumber1', e.target.value);
+        }}
+        autoComplete="cc-number"
+        maxLength={4}
+      />
+      <input
+        type="text"
+        id="cardnumber2"
+        name="cardnumber"
+        onChange={(e) => {
+          console.log('cardnumber2', e.target.value);
+        }}
+        autoComplete="cc-number"
+        maxLength={4}
+      />
+      <input
+        type="text"
+        id="cardnumber3"
+        name="cardnumber"
+        onChange={(e) => {
+          console.log('cardnumber3', e.target.value);
+        }}
+        autoComplete="cc-number"
+        maxLength={4}
+      />
+      <input
+        type="text"
+        id="cardnumber4"
+        name="cardnumber"
+        onChange={(e) => {
+          console.log('cardnumber4', e.target.value);
+        }}
+        autoComplete="cc-number"
+        maxLength={4}
+      />
     </>
   );
 }
