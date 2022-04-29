7a700d3bdac6ce766b1ef1f59359552afdba700e
YIAHN404-FASTFIVE <54733637+ahnanne@users.noreply.github.com>
2022-04-29 16:04:06 +0900

    maxLength 추가

--- pages/payment.js
+++ pages/payment.js
@@ -6,7 +6,10 @@ export default function Payment() {
   return (
     <>
       <label htmlFor="cardnumber1">카드 번호</label>
-      <input type="text" id="cardnumber1" name="cardnumber" value={cardNumber} onChange={(e) => setCardNumber(e.target.value)} autoComplete="cc-number" />
+      <input type="text" id="cardnumber1" name="cardnumber" value={cardNumber} onChange={(e) => {
+        console.log(e.target.value);
+        setCardNumber(e.target.value);
+      }} autoComplete="cc-number" maxLength={4} />
       <input type="text" id="cardnumber2" name="cardnumber" value={cardNumber} onChange={(e) => setCardNumber(e.target.value)} autoComplete="cc-number" />
       <input type="text" id="cardnumber3" name="cardnumber" value={cardNumber} onChange={(e) => setCardNumber(e.target.value)} autoComplete="cc-number" />
       <input type="text" id="cardnumber4" name="cardnumber" value={cardNumber} onChange={(e) => setCardNumber(e.target.value)} autoComplete="cc-number" />
