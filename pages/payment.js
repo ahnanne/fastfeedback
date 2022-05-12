717a64018f163fbd325a0412277e7d8053adcf98
YIAHN404-FASTFIVE <54733637+ahnanne@users.noreply.github.com>
2022-05-12 16:39:53 +0900

    input name을 cardnumber가 아닌 cardNumber로 변경했을 때

--- pages/payment.js
+++ pages/payment.js
@@ -5,52 +5,16 @@ export default function Payment() {
 
   return (
     <>
-      <label htmlFor="cardnumber1">카드 번호</label>
+      <label htmlFor="cardnumber">카드 번호</label>
       <input
+        style={{ border: '1px solid grey' }}
         type="text"
-        id="cardnumber1"
-        name="cardnumber"
+        id="cardnumber"
+        name="cardNumber"
         onChange={(e) => {
-          console.log('cardnumber1', e.target.value);
+          console.log('cardnumber', e.target.value);
         }}
         autoComplete="cc-number"
-        maxLength={4}
-      />
-      <input
-        type="text"
-        id="cardnumber2"
-        name="cardnumber"
-        onChange={(e) => {
-          console.log('cardnumber2', e.target.value);
-        }}
-        autoComplete="cc-number"
-        maxLength={4}
-      />
-      <input
-        type="text"
-        id="cardnumber3"
-        name="cardnumber"
-        onChange={(e) => {
-          console.log('cardnumber3', e.target.value);
-        }}
-        autoComplete="cc-number"
-        maxLength={4}
-      />
-      <input
-        type="text"
-        // id="cardnumber4"
-        // name="cardnumber"
-        onChange={(e) => {
-          console.log('cardnumber4', e.target.value);
-        }}
-        // autoComplete="cc-number"
-        maxLength={4}
-      />
-      <input
-        type="text"
-        onChange={(e) => {
-          console.log('cardnumber4', e.target.value);
-        }}
         maxLength={4}
       />
     </>
